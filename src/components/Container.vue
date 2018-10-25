9d742516d4419d4f9f32b991145931b46b992d91
maymellow <bseeen@gmail.com>
2018-10-25 16:53:32 +0900

    add rank

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,84 +1,148 @@
 <template>
-    <div>
-        <button type="button" @click="startGame">Start</button>
-        <ul>
-            <li v-for="(text, index) in textList" :key="`item-${index}`">
-                <p>{{text}}</p>
-                <p><input type="text" :readonly="activeIndex !== index" :ref="`text`" @keydown.enter="matchText(index, $event)"></p>
-            </li>
-        </ul>
-        <button type="button" @click="endGame" :disabled="!isEnd">Close</button>
-        <span v-if="timeDiffer">{{timeDiffer}}초 경과</span>
-        <ul>
-            <li v-for="history in sortHistoryList">게임 타입 : {{history.type}}, 경과 시간 : {{history.time}}</li>
-        </ul>
-    </div>
+	<div>
+		<input type="text" v-model="userName">
+		<button type="button" @click="startGame" :disabled="!userName.length">Start</button>
+		<div class="container">
+			<div>
+				<template v-if="isStart">
+					<span>{{this.gameHistory[this.gameHistory.findIndex(item => item.id === this.currentId)].time}}</span>
+					<ul>
+						<li v-for="(text, index) in textList" :key="`item-${index}`">
+							<p>{{text}}</p>
+							<p><input type="text" :readonly="activeIndex !== index" :ref="`text`" @keydown.enter="matchText(index, $event)">
+							</p>
+						</li>
+					</ul>
+					<span v-if="currentId">{{this.gameHistory[this.gameHistory.findIndex(item => item.id === this.currentId)].time}}초 걸렸음</span>
+				</template>
+			</div>
+			<div>
+				<ul class="rank__list">
+					<li v-for="(history, index) in sortHistoryList" :key="index">{{index+1}}위, 유저명: {{history.name}}, 게임 타입 : {{history.type}}, 경과 시간 : {{history.time}}</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Container",
-  props: {
-    data: {
-      type: Array,
-      required: true
-    }
-  },
-  data() {
-    return {
-      gameType: null,
-      textList: [],
-      start: null,
-      end: null,
-      timeDiffer: null,
-      activeIndex: -1,
-      isEnd: false,
-      gameHistory: []
-    };
-  },
-  created() {
-    this.newGame();
-    this.gameHistory = JSON.parse(localStorage.getItem("history")) || [];
-  },
-  methods: {
-    newGame() {
-      this.gameType = Math.floor(Math.random() * this.data.length);
-      this.textList = this.data[this.gameType];
-    },
-    startGame() {
-      this.start = new Date().getTime();
-      this.autoFocus(0);
-    },
-    matchText(index, e) {
-      if (this.textList[index] === e.target.value) {
-        index === this.textList.length - 1
-          ? (this.isEnd = true)
-          : this.autoFocus(index + 1);
-      } else {
-        alert("텍스트가 일치하지 않습니다.");
-      }
-    },
-    endGame() {
-      this.end = new Date().getTime();
-      this.timeDiffer = (this.end - this.start) / 1000;
-      this.gameHistory.push({ type: this.gameType, time: this.timeDiffer });
-      localStorage.setItem("history", JSON.stringify(this.gameHistory));
-    },
-    autoFocus(index) {
-      this.activeIndex = index;
-      this.$nextTick(() => {
-        const input = this.$refs.text[index];
-        input.focus();
-      });
-    }
-  },
-  computed: {
-    sortHistoryList: function() {
-      return this.gameHistory.sort((a, b) => a.time - b.time);
-    }
-  }
-};
+	export default {
+		name: "Container",
+		props: {
+			data: {
+				type: Array,
+				required: true
+			}
+		},
+		data () {
+			return {
+				userName: '',
+				gameType: null,
+				textList: [],
+				activeIndex: -1,
+				isStart: false,
+				isEnd: false,
+				gameHistory: [],
+				setTime: null,
+				currentId: ''
+			};
+		},
+		created () {
+			this.newGame();
+			this.gameHistory = JSON.parse(localStorage.getItem("history")) || [];
+		},
+		methods: {
+			newGame () {
+				this.gameType = Math.floor(Math.random() * this.data.length);
+				this.textList = this.data[this.gameType];
+			},
+			startGame () {
+				const uid = 'id-' + Math.random().toString(36).substr(2, 16);
+				this.isStart = true;
+			  this.currentId= uid;
+			  this.gameHistory.push({id: uid, name: this.userName, type: this.gameType, time: 0});
+				this.autoFocus(0);
+				this.setTimer();
+			},
+			setTimer () {
+				this.setTime = setInterval(() => {
+					this.gameHistory.map((item) => {
+						if(item.id=== this.currentId){
+							item.time += 1;
+						}
+					});
+				}, 1000)
+			},
+			matchText (index, e) {
+				if (this.textList[index] === e.target.value) {
+					index === this.textList.length - 1
+						? this.endGame()
+						: this.autoFocus(index + 1);
+				} else {
+					alert("텍스트가 일치하지 않습니다.");
+				}
+			},
+			endGame () {
+				clearInterval(this.setTime);
+				this.userName='';
+		  	this.currentId= '';
+		  	this.isStart=false;
+				localStorage.setItem("history", JSON.stringify(this.gameHistory));
+			},
+			autoFocus (index) {
+				this.activeIndex = index;
+				this.$nextTick(() => {
+					const input = this.$refs.text[index];
+					input.focus();
+				});
+			}
+		},
+		computed: {
+			sortHistoryList: function () {
+				const game = this.gameHistory;
+				return game.sort((a, b) => a.time - b.time || a.type - b.type);
+			}
+		}
+	};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+	* {
+		box-sizing: border-box;
+	}
+	input[type="text"]{
+		height: 25px;
+		vertical-align: middle;
+	}
+	button{
+		vertical-align: middle;
+		border-radius: 5px;
+		font-size: 14px;
+		cursor: pointer;
+		line-height: 25px;
+		padding: 0 20px;
+		color: white;
+		background-color: black;
+		&:disabled{
+			opacity: 0.8;
+	 	}
+	}
+	.container{
+		width: 800px;
+		display: flex;
+		align-content: space-between;
+	}
+	li {
+		padding: 0;
+		list-style: none;
+		margin: 0;
+	}
+	.rank__list{
+		margin-left: 30px;
+		border: 1px solid #eee;
+		padding: 20px;
+		li{
+			line-height: 25px;
+		}
+	}
 </style>
